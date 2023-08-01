f6383e2b4dd9f9f8ba49bf6b4fb31a4006e39830
Lorenzo Loi <51636003+Lokze@users.noreply.github.com>
2023-08-01 16:52:40 +0200

    don't like this

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,11 +1,45 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react';
+import { RecipesContext } from './recipesContext';
+import { Link } from 'react-router-dom';
+
+const Cards = () => {
+  console.log('Cards component is rendered.');
+  
+  const { recipes } = useContext(RecipesContext);
+  console.log('Recipes data in Cards.js:', recipes);
+
+  if (!recipes || recipes.length === 0) {
+    return <div>No recipes found.</div>;
+  }
+
+  return (
+    <div>
+      <ul id='cards' className='card-container'>
+        {recipes.map((recipe) => (
+          <li className='card' key={recipe.id}>
+            <p><span>{recipe.title}</span></p>
+            <img src={recipe.image} alt={recipe.title} />
+            <Link to={`/full-recipe/${recipe.id}`}>
+              <button className="green-button">View Recipe</button>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Cards;
+
+
+/*import React, { useContext, useState } from 'react'
 import { RecipesContext } from './recipesContext'
 import { Link } from 'react-router-dom';
 import FullRecipe from './FullRecipe';
 
 const Cards = () => {
     const {recipes} = useContext(RecipesContext);
-
+    console.log('Recipes data in Cards.js:', recipes);
     console.log(recipes);
 
   return (
@@ -15,7 +49,7 @@ const Cards = () => {
         <li className='card' key={recipe.id}>
           <p><span>{recipe.title}</span></p>
           <img src={recipe.image} alt={recipe.title} />
-          <Link to="./FullRecipe.js">
+          <Link to="/full-recipe">
           <a  className="green-button">View Recipe</a>
           </Link>
         </li>
@@ -25,4 +59,4 @@ const Cards = () => {
   )
 }
 
-export default Cards
+export default Cards*/
